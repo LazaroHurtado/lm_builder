Fix loss computation in transformer class

lm_builder/transformer/transformer.py
@@ -56,7 +56,9 @@ class Transformer(nn.Module):
         loss = None
         if targets is not None:
             loss = F.cross_entropy(
-                logits.view(-1).unsqueeze(0), targets.view(-1), ignore_index=-1
+                logits.reshape(-1, logits.size(-1)),
+                targets.reshape(-1),
+                ignore_index=-1,
             )
 
         return logits, loss